bbff524175e1ab41c360f13d1ec4a26c9369bed8
alexesp <aleizaes@hotmail.com>
2025-03-05 15:59:06 +0100

    Crear biblioteca UseCases

---
+++ InventoryManagementSystem.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.UseCases.Inventories
+{
+    public class ViewInventoriesByNameUseCase
+    {
+        public IEnumerable<Inventory> ExecuteAsync(string name = "");
+    }
+}
